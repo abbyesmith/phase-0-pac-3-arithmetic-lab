db7941ee526cd7783ca2bc1d246e0855bb3bc276
Abby <abby.e.smith.85@gmail.com>
2023-01-19 07:40:34 -0700

    commit tree ee54f309f22ad9b36d8da05e8f893b34f2a5aec4

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a,b){
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b
+}
+
+function multiply(a,b){
+    return a*b
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+let number = 5
+
+function increment(number){
+    return number+=1;
+}
+increment();
+
+function decrement(number){
+    return number-= 1;
+}
+decrement();
+
+//parseInt
+function makeInt(number){
+    return parseInt(number, 10);
+}
+
+function preserveDecimal(number){
+    return parseFloat(number);
+}
+
